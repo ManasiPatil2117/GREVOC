98e98cc3b8fcd9de81f3a3361cd35468f6570166
Manasi <manasipatilap2117@gmail.com>
2023-04-15 19:41:21 +0530

    DB_STRING env variable

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+
 const connectDB = async () => {
     try {
-        const con = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bgfrxob.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`);
-        console.log("Connected to db");
+        const dbString = process.env.DB_STRING;
+        if (!dbString) {
+            throw new Error("DB_STRING environment variable is not defined");
+        }
+        const con = await mongoose.connect(dbString);
+        console.log("Connected to database");
     } catch (error) {
-        console.log("Error in connecting DB: " + error)
+        console.error("Error in connecting to database:", error.message);
+        process.exit(1);
     }
-}
+};
+
 module.exports = { connectDB, mongoose };
