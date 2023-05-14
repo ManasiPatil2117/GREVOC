9f2c444f10588b693d9cb2e7b85e3d58bfc72ca3
Manasi <manasipatilap2117@gmail.com>
2023-05-14 10:11:13 +0530

    name modification

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -49,7 +49,7 @@ export default function Dashboard() {
 
                   <motion.div className="flex flex-col rounded-lg shadow-lg overflow-hidden" whileHover={{ scale: 1.1 }}>
                     <div className="flex-shrink-0">
-                      <img className="h-48 w-full object-cover" src={quiz} alt="Person reading book" />
+                      <img className="h-48 w-full object-cover" src={quiz} alt="Quiz image" />
                     </div>
                     <div className="flex-1 bg-white p-6 flex flex-col justify-between">
                       <div className="flex-1">
@@ -72,7 +72,7 @@ export default function Dashboard() {
                   </motion.div>
                   <motion.div className="flex flex-col rounded-lg shadow-lg overflow-hidden" whileHover={{ scale: 1.1 }}>
                     <div className="flex-shrink-0">
-                      <img className="h-48 w-full object-cover" src={download} alt="Person holding pen" />
+                      <img className="h-48 w-full object-cover" src={download} alt="Download image" />
                     </div>
                     <div className="flex-1 bg-white p-6 flex flex-col justify-between">
                       <div className="flex-1">
